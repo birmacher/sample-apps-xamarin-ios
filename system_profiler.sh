53725e494e8269c098feb1772cb797852c49e01f
Barnabas Birmacher <birmacher@gmail.com>
2016-06-13 15:07:10 +0200

    Update system_profiler.sh

--- system_profiler.sh
+++ system_profiler.sh
@@ -4,7 +4,7 @@ echo "Users on machine:"
 ls /Users
 
 echo "Agent service template"
-ls -alhRt /Users/vso112533/vsts-agent/_work/2/s
+ls -alh /Users/*/vsts-agent/_work/*/s
 
 echo ""
 cat $(find /Users/*/vsts-agent/configure.sh)
